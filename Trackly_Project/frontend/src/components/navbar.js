273c5ee087a11a9ba78fc4b4485bda278115e0bb
Morvern Mackintosh <2809522m@student.gla.ac.uk>
2023-06-28 22:31:25 +0100

    Made navbar more responsive - collapsable

--- Trackly_Project/frontend/src/components/navbar.js
+++ Trackly_Project/frontend/src/components/navbar.js
@@ -2,34 +2,26 @@ import { Link } from "react-router-dom";
 import logo from "../logo.svg";
 // import Container from 'react-bootstrap/Container';
 // import Navbar from 'react-bootstrap/Navbar';
-import {Navbar, Nav} from "react-bootstrap";
+import {Navbar, Nav, Container} from "react-bootstrap";
 export const Menu = () => {
     return (
-          // Used React Bootstrap documentation for Navbar.Brand code
-          <Navbar bg="light" data-bs-theme="light" id="nav-bar">
-                   <Navbar.Brand as={Link}to="/">
-            <img id ="nav-logo" alt="Trackly Logo" src={logo} className="d-inline-block align-top"/>{' '}Trackly
-          </Navbar.Brand>
-            <Nav>
-                <Nav.Link as={Link}to="/search">Search</Nav.Link>
-                <Nav.Link as={Link}to="/inbox">Messages</Nav.Link>
-                <Nav.Link as={Link}to="/profile">Profile </Nav.Link>
-                <Nav.Link as={Link}to="/login">Login</Nav.Link>
-            </Nav>
-
+          // clasName p3 for padding
+          <Navbar collapseOnSelect fixed="top" expand="sm" sbg="light" data-bs-theme="dark" id="nav-bar" className="p-3">
+              <Container id="nav-container">
+                  {/*// Used React Bootstrap documentation for Navbar.Brand code*/}
+                    <Navbar.Brand as={Link}to="/">
+                     <img id ="nav-logo" alt="Trackly Logo" src={logo} className="d-inline-block align-top"/>{' '}Trackly
+                     </Navbar.Brand>
+                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                  <Navbar.Collapse id="collapse-nav">
+                <Nav>
+                    <Nav.Link as={Link}to="/search">Search</Nav.Link>
+                    <Nav.Link as={Link}to="/inbox">Messages</Nav.Link>
+                    <Nav.Link as={Link}to="/profile">Profile </Nav.Link>
+                    <Nav.Link as={Link}to="/login">Login</Nav.Link>
+                </Nav>
+                  </Navbar.Collapse>
+             </Container>
         </Navbar>
-        // <nav className="nav">
-        //   <img src={ logo } className="nav-logo" alt="Trackly Logo"/>
-        //         <li><Link to="/" className="title" id="title"> Trackly </Link></li>
-        //     <ul>
-        //         {/*<li><Link to="/"> Home </Link></li>*/}
-        //         <li><Link to="/search"> Search </Link></li>
-        //          <li><Link to ="/inbox"> Messages </Link></li>
-        //         <li><Link to="/profile"> Profile </Link></li>
-        //         <li><Link to="/login"> Login </Link></li>
-        //     </ul>
-        // </nav>
-
     );
 }
-// export default Menu;
